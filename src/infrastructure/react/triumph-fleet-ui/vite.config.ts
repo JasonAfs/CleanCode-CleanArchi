83afdc334d649cc595cecd99f87d97876f10dcfe
Afonso Jason <jason.afonso.fernandes@gmail.com>
2025-01-22 19:05:51 +0100

    install react succesfully

---
+++ src/infrastructure/react/triumph-fleet-ui/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+  },
+  resolve: {
+    alias: {
+      '@domain': path.resolve(__dirname, '../../../../domain'),
+      '@application': path.resolve(__dirname, '../../../../application'),
+      '@infrastructure': path.resolve(__dirname, '../../../'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
+})
