cb2da092d9331dd9638412ed2a3ee430bcb75bf5
Afonso Jason <jason.afonso.fernandes@gmail.com>
2025-02-08 19:14:05 +0100

    fix update mileage for admin

--- src/application/use-cases/motorcycle/UpdateMotorcycleMileageUseCase.ts
+++ src/application/use-cases/motorcycle/UpdateMotorcycleMileageUseCase.ts
@@ -6,67 +6,78 @@ import { Result } from '@domain/shared/Result';
 import { MotorcycleNotFoundError } from '@domain/errors/motorcycle/MotorcycleValidationError';
 import { UnauthorizedError } from '@domain/errors/authorization/UnauthorizedError';
 import { UpdateMotorcycleMileageResponseDTO } from '@application/dtos/motorcycle/response/UpdateMotorcycleMileageResponseDTO';
+import { UserRole } from '@domain/enums/UserRole';
 
 export class UpdateMotorcycleMileageUseCase {
-    private readonly validator = new UpdateMotorcycleMileageValidator();
+  private readonly validator = new UpdateMotorcycleMileageValidator();
 
-    constructor(
-        private readonly motorcycleRepository: IMotorcycleRepository,
-        private readonly dealershipRepository: IDealershipRepository,
-    ) {}
+  constructor(
+    private readonly motorcycleRepository: IMotorcycleRepository,
+    private readonly dealershipRepository: IDealershipRepository,
+  ) {}
 
-    public async execute(
-        dto: UpdateMotorcycleMileageDTO,
-    ): Promise<Result<UpdateMotorcycleMileageResponseDTO, Error>> {
-        try {
-            // 1. Validation des données d'entrée et permissions
-            try {
-                this.validator.validate(dto);
-            } catch (error) {
-                if (error instanceof Error) {
-                    return error;
-                }
-                throw error;
-            }
-
-            // 2. Récupération de la moto
-            const motorcycle = await this.motorcycleRepository.findById(dto.motorcycleId);
-            if (!motorcycle) {
-                return new MotorcycleNotFoundError(dto.motorcycleId);
-            }
-
-            // 3. Vérification que l'employé appartient à la bonne concession
-            if (!motorcycle.dealershipId || motorcycle.dealershipId !== dto.userDealershipId) {
-                return new UnauthorizedError("You don't have access to update this motorcycle's mileage");
-            }
-
-            const previousMileage = motorcycle.mileage;
-            // 4. Mise à jour du kilométrage
-            try {
-                motorcycle.updateMileage(dto.mileage);
-            } catch (error) {
-                if (error instanceof Error) {
-                    return error;
-                }
-                throw error;
-            }
-
-            // 5. Persistence
-            await this.motorcycleRepository.updateMileage(motorcycle.id, dto.mileage);
-
-            // 6. Retour de la réponse
-            return {
-                success: true,
-                message: `Motorcycle mileage has been successfully updated`,
-                motorcycleId: motorcycle.id,
-                previousMileage: previousMileage,
-                newMileage: dto.mileage
-            };
-        } catch (error) {
-            if (error instanceof Error) {
-                return error;
-            }
-            return new Error('An unexpected error occurred while updating motorcycle mileage');
+  public async execute(
+    dto: UpdateMotorcycleMileageDTO,
+  ): Promise<Result<UpdateMotorcycleMileageResponseDTO, Error>> {
+    try {
+      // 1. Validation des données d'entrée et permissions
+      try {
+        this.validator.validate(dto);
+      } catch (error) {
+        if (error instanceof Error) {
+          return error;
         }
+        throw error;
+      }
+
+      // 2. Récupération de la moto
+      const motorcycle = await this.motorcycleRepository.findById(
+        dto.motorcycleId,
+      );
+      if (!motorcycle) {
+        return new MotorcycleNotFoundError(dto.motorcycleId);
+      }
+      console.log('user role = ' + dto.userRole);
+      // 3. Vérification que l'employé appartient à la bonne concession
+      if (
+        dto.userRole !== UserRole.TRIUMPH_ADMIN &&
+        (!motorcycle.dealershipId ||
+          motorcycle.dealershipId !== dto.userDealershipId)
+      ) {
+        return new UnauthorizedError(
+          "You don't have access to update this motorcycle's mileage",
+        );
+      }
+
+      const previousMileage = motorcycle.mileage;
+      // 4. Mise à jour du kilométrage
+      try {
+        motorcycle.updateMileage(dto.mileage);
+      } catch (error) {
+        if (error instanceof Error) {
+          return error;
+        }
+        throw error;
+      }
+
+      // 5. Persistence
+      await this.motorcycleRepository.updateMileage(motorcycle.id, dto.mileage);
+
+      // 6. Retour de la réponse
+      return {
+        success: true,
+        message: `Motorcycle mileage has been successfully updated`,
+        motorcycleId: motorcycle.id,
+        previousMileage: previousMileage,
+        newMileage: dto.mileage,
+      };
+    } catch (error) {
+      if (error instanceof Error) {
+        return error;
+      }
+      return new Error(
+        'An unexpected error occurred while updating motorcycle mileage',
+      );
     }
+  }
 }
